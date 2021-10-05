0d11adc2a7a2cabd4e1b7a9ae81cab2196dddba1
irapaige <38634811+irapaige@users.noreply.github.com>
2021-10-05 06:53:36 -0400

    dded redux toolkit to ome screen

--- amazon/src/screens/HomeScreen.js
+++ amazon/src/screens/HomeScreen.js
@@ -1,32 +1,28 @@
 import React, { useEffect } from 'react';
 
 import Product from '../Components/product';
-import LoadingBox from '../Components/loadingBox';
-import MessageBox from '../Components/messageBox';
+
+
 import {useDispatch, useSelector} from "react-redux";
-import {listProducts} from'../actions/productActions'
+import {getProducts} from '../features/products/productSlice'
 
 export default function HomeScreen() {
     const dispatch =useDispatch();
- const productList =useSelector(state =>state.productList)
-const {loading,error,products} = productList;
+ const {products} =useSelector(state =>state.products)
+
 
     useEffect(() => {
-       dispatch(listProducts())
+       dispatch(getProducts())
     }, []);
     return (
         <div>
-            {loading ? (
-                <LoadingBox></LoadingBox>
-            ) : error ? (
-                <MessageBox variant="danger">{error}</MessageBox>
-            ) : (
+
                 <div className="row center">
                     {products.map((product) => (
                         <Product key={product._id} product={product}></Product>
                     ))}
                 </div>
-            )}
+            }
         </div>
     );
 }
