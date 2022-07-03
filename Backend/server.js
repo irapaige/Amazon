cf175f22965235b7fc988e17f622afd46a030fb3
irapaige <38634811+irapaige@users.noreply.github.com>
2022-07-03 18:46:04 -0400

    added products from mongoDB

--- Backend/server.js
+++ Backend/server.js
@@ -1,32 +1,24 @@
 import express from 'express';
 import data from './data.js';
 import mongoose from 'mongoose';
-import seedRouter from "./routes/seedRoutes.js";
-import Product from "./models/productModel.js"
-import productRouter from "./routes/productRoutes.js";
+import seedRouter from './routes/seedRoutes.js';
+import Product from './models/productModel.js';
+import productRouter from './routes/productRoutes.js';
 
-
-
-
-mongoose.connect("mongodb+srv://Irapaige:Mb06081959@my-profile.zgj4a.mongodb.net/Amazon?retryWrites=true&w=majority"
-).then(()=>{
-  console.log('connected to DB');
-})
-    .catch((err) => {
-      console.log(err.message);
-    });
-
+mongoose
+  .connect(
+    'mongodb+srv://Irapaige:Mb06081959@my-profile.zgj4a.mongodb.net/Amazon?retryWrites=true&w=majority'
+  )
+  .then(() => {
+    console.log('connected to DB');
+  })
+  .catch((err) => {
+    console.log(err.message);
+  });
 
 const app = express();
-app.use('/api/seed',seedRouter);
-app.use('/api/products',productRouter)
-
-
-
-
-
-
-
+app.use('/api/seed', seedRouter);
+app.use('/api/products', productRouter);
 
 app.get('/', (req, res) => {
   res.send('Server is ready');
